d2831eeae89263f12682dcf76c9ff67dbd62749e
jh <jh>
2018-05-20 14:31:17 +0800

    discover

--- src/scenes/sample/Sample.js
+++ src/scenes/sample/Sample.js
@@ -4,11 +4,29 @@ import './Sample.css'
 class Sample extends PureComponent {
   render () {
     return (
-      <div className="scene-sample">
-        <h1>Wow very sample</h1>
-      </div>
+        <div className="wrapper">
+            <div className="header">
+                <div className="navBar">
+                    <h1 className="title">LEARNING JOURNEY</h1>
+                </div>
+            </div>
+            <div className="container">
+                <input type="text" className="searchBar" placeholder="Search" onKeyPress={this.keypress}/>
+                <div className =  "results">
+                    <div className = "results1"></div>
+                    <div className = "results2"></div>
+                    <div className = "results3"></div>
+                </div>
+            </div>
+        </div>
     )
   }
+  keypress (e){
+    if(e.key == 'Enter'){
+        window.location.href = "/results/python"
+    }
+  }
 }
+
 
 export default Sample
